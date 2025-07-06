c3230d73f4b040b7510fa41248a68cbaf314a537
Chris <cdelicaat.me@gmail.com>
2025-07-06 14:04:25 +0200

    Removed pause from shader compile script

--- Engine/Shaders/compile_shaders.bat
+++ Engine/Shaders/compile_shaders.bat
@@ -1,4 +1,3 @@
 @REM forfiles /s /m *.vert /c "cmd /c %VULKAN_SDK%/Bin/glslangValidator.exe @path -gVS -V -o @fname.vert.spv"
 @REM forfiles /s /m *.frag /c "cmd /c %VULKAN_SDK%/Bin/glslangValidator.exe @path -gPS -V -o @fname.frag.spv"
 forfiles /s /m *.comp /c "cmd /c %VULKAN_SDK%/Bin/glslangValidator.exe @path -gCS -V -o @fname.comp.spv"
-pause
