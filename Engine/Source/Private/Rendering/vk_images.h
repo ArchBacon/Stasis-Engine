15425375c900c3209828f6c345b83b4c8b5eab65
Chris <cdelicaat.me@gmail.com>
2025-07-09 02:26:48 +0200

    Improved render loop

--- Engine/Source/Private/Rendering/vk_images.h
+++ Engine/Source/Private/Rendering/vk_images.h
@@ -4,5 +4,5 @@
 
 namespace Stasis::vkutil {
     void TransitionImage(VkCommandBuffer command, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
-    void CopyImageToImage(VkCommandBuffer command, VkImage source, VkImage destination, VkExtent2D sourceSize, VkExtent2D dstSize);
+    void CopyImageToImage(VkCommandBuffer command, VkImage source, VkImage destination, VkExtent2D srcSize, VkExtent2D dstSize);
 };
