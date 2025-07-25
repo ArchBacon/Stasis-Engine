9223523342724d6869c2ecbe24ceed2f516c39c9
Chris <cdelicaat.me@gmail.com>
2025-07-25 15:06:08 +0200

    rainbow triangle!

--- Engine/Shaders/basic.frag
+++ Engine/Shaders/basic.frag
@@ -2,9 +2,9 @@
 
 out vec4 outColor;
 
-uniform vec4 vertexColor;
+in vec3 vertexColor;
 
 void main()
 {
-    outColor = vertexColor;
+    outColor = vec4(vertexColor, 1.0);
 }
--- Engine/Source/Private/Graphics/GlRenderer.cpp
+++ Engine/Source/Private/Graphics/GlRenderer.cpp
@@ -64,16 +64,15 @@ namespace blackbox::graphics
         // Create Triangle & Co.
         float vertices[]
         {
-            0.5f,  0.5f, 0.0f,  // top right
-            0.5f, -0.5f, 0.0f,  // bottom right
-           -0.5f, -0.5f, 0.0f,  // bottom left
-           -0.5f,  0.5f, 0.0f   // top left 
+            // positions        // colors
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
+           -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
+            0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
         };
         
         unsigned indices[]
         {
-            0, 1, 3,   // first triangle
-            1, 2, 3    // second triangle
+            0, 1, 2,
         };
 
         // Create, bind, and pass data to Vertex Buffer Object
@@ -91,8 +90,11 @@ namespace blackbox::graphics
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
         
         // Set vertex attributes
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
         glEnableVertexAttribArray(0);
+        
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+        glEnableVertexAttribArray(1);
 
         glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind VBO
         glBindVertexArray(0);    
@@ -114,14 +116,8 @@ namespace blackbox::graphics
         // Wireframe mode
         // glPolygonMode(GL_FRONT_AND_BACK, GL_POINT); // GL_FILL for default
         
-        float timeValue = ::Engine.Uptime();
-        float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "vertexColor");
-
         glUseProgram(shaderProgram);
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
-        
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
     }
 }
